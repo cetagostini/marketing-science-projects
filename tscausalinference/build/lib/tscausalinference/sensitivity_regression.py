c93a1d8767014570fc7b2cbb5fadb1b5720d18bd
Carlos Trujillo <carlangastr@gmail.com>
2023-03-18 01:25:20 +0200

    --Version 5.4

--- tscausalinference/build/lib/tscausalinference/sensitivity_regression.py
+++ tscausalinference/build/lib/tscausalinference/sensitivity_regression.py
@@ -160,8 +160,7 @@ def sensitivity_analysis(df: DataFrame = pd.DataFrame(),
             test_mask = (temp_test['ds'] >= pd.to_datetime(test_period[0])) & (temp_test['ds'] <= pd.to_datetime(test_period[1]))
             
             #mask last 90 days
-            rolling_mask = (temp_test['ds'] <= (pd.to_datetime(test_period[0]) - pd.Timedelta(days = 89)))
-
+            rolling_mask = (temp_test['ds'] >= (pd.to_datetime(test_period[0]) - pd.Timedelta(days = 89))) & (temp_test['ds'] < pd.to_datetime(test_period[0]))
             # multiply 'y' by 1.1 in the test period
             temp_test.loc[test_mask, 'y'] *= effect
             
@@ -178,7 +177,7 @@ def sensitivity_analysis(df: DataFrame = pd.DataFrame(),
                                                                                         )
             
             results_df = pd.DataFrame({'injected_effect':[round(effect, 2)], 'model':[model_parameters], 'pvalue':[stadisticts[0]], 'train': [training], 
-                                       'test_': [test], 'intervention': [int_metrics], 'confidence_interval':[temp_test.yhat_lower().sum(), temp_test.yhat_upper.sum()],
+                                       'test_': [test], 'intervention': [int_metrics], 'confidence_interval':[temp_test.yhat_lower.sum(), temp_test.yhat_upper.sum()],
                                        'y_intervention': [temp_test[test_mask].y.sum()], 'mean_intervention': [temp_test[test_mask].y.mean()],
                                        'last90days_mean': [temp_test[rolling_mask].y.mean()], 'historical_mean': [temp_test[temp_test['ds'] < pd.to_datetime(test_period[0])].y.mean()] 
                                        })
--- tscausalinference/tscausalinference/__version__.py
+++ tscausalinference/tscausalinference/__version__.py
@@ -13,4 +13,4 @@
 # limitations under the License.
 
 
-__version__ = '0.1.5.3'
+__version__ = '0.1.5.4'
